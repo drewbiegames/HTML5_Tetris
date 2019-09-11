5608d5da9332bc382bed02421bf7bc9093e10ff2
Drew Bowden <31940413+drewbiegames@users.noreply.github.com>
2019-09-11 16:10:38 +0100

    Completed with bug and no pretty gui: If you hold down key the causes undesired behaviour.

---
+++ Piece.js
@@ -0,0 +1,151 @@
+// Object : Piece
+function Piece(tetromino, color){
+	this.tetromino = tetromino;
+	this.color = color;
+
+	this.tetrominoNo = 0; // first pattern
+	this.activeTetromino = this.tetromino[this.tetrominoNo];
+	
+	this.x = 3;
+	this.y = -2;
+}
+
+//pass false to undraw the tetromino
+Piece.prototype.drawTertomino = function(draw) {
+	for (r = 0; r < this.activeTetromino.length; r++){
+		for (c = 0; c < this.activeTetromino.length; c++){
+			// draw only occupied squares
+			if(this.activeTetromino[r][c]){
+				if (draw){
+					drawSquare(this.x + c, this.y + r, this.color);
+				}
+				else {
+					drawSquare(this.x + c, this.y + r, VACANT);
+				}
+			}
+		}
+	}
+}
+
+Piece.prototype.moveDown = function(score, vacant){
+	if(!this.checkCollison(0,1,this.activeTetromino)){
+		//undraw to stop drawing piece copying
+		this.drawTertomino(false);
+		this.y++;
+		//draw new position
+		this.drawTertomino(true);
+		return {collide: false, points: score};
+	}
+	else {
+		return {collide: true, points: this.lock(score, vacant)};
+	}
+}
+
+Piece.prototype.moveLeft = function(){
+	if(!this.checkCollison(-1,0,this.activeTetromino)){
+		//clear previous position
+		this.drawTertomino(false);
+		this.x--;
+		this.drawTertomino(true);
+	}
+}
+
+Piece.prototype.moveRight = function(){	
+	if(!this.checkCollison(1,0,this.activeTetromino)){
+		//clear previous position 
+		this.drawTertomino(false);
+		this.x++;
+		this.drawTertomino(true);
+	}
+}
+
+Piece.prototype.rotate = function(){
+	let nextPattern = this.tetromino[(this.tetrominoNo + 1) % this.tetromino.length];
+	let kick = 0;
+	if (this.checkCollison(0,0,nextPattern)){
+		if (this.x > COLUMN / 2){
+		//right wall
+		kick = -1;
+	} else {
+		//left wall
+		kick = 1;
+	}
+}
+if(!this.checkCollison(kick,0,nextPattern)){
+		//clear previous position
+		this.drawTertomino(false);
+		this.x += kick;
+		this.tetrominoNo = (this.tetrominoNo + 1)% this.tetromino.length;
+		this.activeTetromino = this.tetromino[this.tetrominoNo];
+		this.drawTertomino(true);
+	}
+	
+}
+
+Piece.prototype.checkCollison = function(xIncrement,yIncrement, pattern){
+
+	for (r = 0; r < pattern.length; r++){
+		for (c = 0; c < pattern.length; c++){
+			// occupied square?
+			if(!pattern[r][c]){
+				continue;
+			}
+
+			let newX = this.x + c + xIncrement;
+			let newY = this.y + r + yIncrement;
+
+			if(newX < 0 || newX >= COLUMN || newY >= ROW){
+				return true;
+			}
+			if (newY < 0){
+				continue;
+			}
+			//check for locked piece
+			if(board[newY][newX] != VACANT){
+				return true;
+			}
+		}
+	}
+	return false;
+	
+}
+
+Piece.prototype.lock = function(score, vacant) {
+
+	let scr = score;
+	for (r = 0; r < this.activeTetromino.length; r++){
+		for (c = 0; c < this.activeTetromino.length; c++){
+			//skip vacant squares
+			if(!this.activeTetromino[r][c]){
+				continue;
+			}
+			if(this.y + r < 0){
+				gameOver = true;
+				alert("Game Over!");
+				break;
+			}
+			board[this.y + r][this.x + c] = this.color;
+		}
+	}
+	//remove full rows
+	for (r=0; r < ROW; r++){
+		let isRowFull = true;
+		for(c = 0; c < COLUMN; c++){
+			isRowFull = isRowFull && (board[r][c] != vacant);
+		}
+		if (isRowFull){
+			for (y = r; y > 1; y--){
+				for(c=0; c < COLUMN; c++){
+					board[y][c] = board[y-1][c];
+				}
+			}
+			for(c=0; c < COLUMN; c++){
+				board[0][c] = VACANT;
+			}
+			scr += 10;
+		}
+
+	}
+	drawBoard();
+	return scr;
+}
---
+++ tetris.js
@@ -0,0 +1,115 @@
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+const scoreElement = document.getElementById('score');
+
+const SQ = 20;
+const ROW = 20;
+const COLUMN = 10;
+const VACANT = "WHITE";
+const PIECES = [
+		[Z, "red"],
+		[S, "green"],
+		[T, "cyan"],
+		[O, "indigo"], 
+		[I, "blue"], 
+		[L, "purple"], 
+		[J, "orange"]
+	];
+
+function drawSquare(posX, posY, fill){
+	context.fillStyle = fill;
+	context.fillRect(posX*SQ, posY*SQ, SQ, SQ);
+	context.strokeStyle = "BLACK";
+	context.strokeRect(posX*SQ, posY*SQ, SQ, SQ);
+
+}
+
+//create board
+
+let board = [];
+for (r = 0; r < ROW; r++){
+	board[r] = [];
+	for (c = 0; c < COLUMN; c++){
+		board[r][c] = VACANT;
+	}
+}
+
+//draw board
+function drawBoard() {
+	for (r = 0; r < ROW; r++){
+		for (c = 0; c < COLUMN; c++){
+			drawSquare(c, r, board[r][c]);
+		}
+	}
+}
+
+drawBoard();
+
+function randomPiece(){
+	let randonNo = Math.floor(Math.random() * PIECES.length);
+
+	return new Piece(PIECES[randonNo][0], PIECES[randonNo][1]);
+}
+
+let piece = randomPiece();
+let dropStart = Date.now();
+let gameOver = false;
+piece.drawTertomino(true);
+
+let score = 0;
+
+function drop() {
+	let now = Date.now();
+	delta = now - dropStart;
+
+	if(delta > 1000){
+		moveDownResults = piece.moveDown(score, VACANT);
+		score = moveDownResults.points;
+		scoreElement.innerHTML = score;
+		console.log(score);
+		if (moveDownResults.collide) {
+			piece = randomPiece();
+		}
+		dropStart = Date.now();
+	}
+	if (!gameOver){
+		requestAnimationFrame(drop);
+	}
+}
+drop();
+
+//control
+
+const Keys = {
+	LEFT: 37,
+	UP: 38,
+	RIGHT: 39,
+	DOWN: 40
+}
+
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event){
+	document.removeEventListener("keydown", CONTROL);
+	switch (event.keyCode) {
+		case Keys.LEFT:
+		piece.moveLeft();
+		dropStart = Date.now();
+		break;
+		case Keys.RIGHT:
+		piece.moveRight();
+		dropStart = Date.now();
+		break;
+		case Keys.UP:
+		piece.rotate();
+		dropStart = Date.now();
+		break;
+		case Keys.DOWN:
+		piece.moveDown();
+		score = moveDownResults.points;
+		console.log(score);
+		dropStart = Date.now();
+		break;
+	}
+	document.addEventListener("keydown", CONTROL);
+}
